e636e335f8d5af8ddec96585a01de233aebbfdd0
gregor <info@gregorkofler.com>
2026-04-09 16:24:25 +0200

    Fix: Move options for rendering to component props

--- src/QrCode.vue
+++ src/QrCode.vue
@@ -1,30 +1,26 @@
 <script setup>
-  import { useStringToQrMatrix } from './useStringToQrMatrix'
+  import { stringToQrMatrix, defaultOptions } from './stringToQrMatrix'
   import { computed } from 'vue'
 
-  const props = defineProps({ text: { type: String, required: true }, options: { type: Object, default: () => ({}) }})
-  const { matrix } = useStringToQrMatrix(props.text)
-  const defaultOptions = {
-    gutter: 4,
-    moduleSize: 10,
-    svgAsPath: true
-  }
+  const props = defineProps({
+    text: { type: String, required: true },
+    options: { type: Object, default: () => ({}) },
+    gutter: { type: Number, default: 4 },
+    moduleSize: { type: Number, default: 10 },
+    svgAsPath: { type: Boolean, default: true },
+  })
   const mergedOptions = computed(() => ({...defaultOptions, ...props.options }))
-  const totalSize = computed(() => (matrix.value.length + 2 * mergedOptions.value.gutter) * mergedOptions.value.moduleSize)
+  const matrix = computed(() => stringToQrMatrix(props.text, mergedOptions.value))
+  const totalSize = computed(() => (matrix.value.length + 2 * props.gutter) * props.moduleSize)
   const path = computed(() => {
-
-    const { gutter, moduleSize, svgAsPath } = mergedOptions.value
+    const { gutter, moduleSize, svgAsPath } = props
     const size = matrix.value.length
     let path = ''
-
     for (let r = 0; r < size; ++r) {
       for (let c = 0; c < size; ++c) {
         if (matrix.value[r][c]) {
           let x = (c + gutter) * moduleSize, y = (r + gutter) * moduleSize
-          path +=
-              svgAsPath ?
-                  `M${x},${y}h${moduleSize}v${moduleSize}h-${moduleSize}z` :
-                  `<rect x="${x}" y="${y}" width="${moduleSize}" height="${moduleSize}" />`
+          path += svgAsPath ? `M${x},${y}h${moduleSize}v${moduleSize}h-${moduleSize}z` : `<rect x="${x}" y="${y}" width="${moduleSize}" height="${moduleSize}" />`
         }
       }
     }
@@ -38,7 +34,7 @@
       :viewBox="'0 0 ' + totalSize + ' ' + totalSize" :width="totalSize" :height="totalSize"
   >
     <rect width="100%" height="100%" fill="white" />
-    <path v-if="mergedOptions.svgAsPath" :d="path" fill="black" />
-    <g v-else fill="black">{{ path }}</g>
+    <path v-if="svgAsPath" :d="path" fill="black" />
+    <g v-else fill="black" v-html="path"></g>
   </svg>`
 </template>
